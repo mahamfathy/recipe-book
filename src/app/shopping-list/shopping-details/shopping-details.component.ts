25352769c6785778bf95fe8954001780f62a1115
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-25 00:33:44 +0200

    building the structure

---
+++ src/app/shopping-list/shopping-details/shopping-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './shopping-details.component.html',
+  styleUrl: './shopping-details.component.css'
+})
+export class ShoppingDetailsComponent {
+
+}

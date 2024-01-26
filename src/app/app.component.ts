7535812718fec3017bebdec9b5c82dfd7b15773d
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-27 01:36:57 +0200

    adding to app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,RecipesComponent,ShoppingListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'recipe-book';
+  loadedFeature:string='recipe'
+  onNavigate(feature:string){
+    this.loadedFeature=feature
+  }
 }

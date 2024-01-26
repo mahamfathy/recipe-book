2feae1ff1927da73fbf2b62b4c8459a0e1d01c90
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-27 01:50:10 +0200

    editing in recipes comp

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { FormsModule } from '@angular/forms';
+import { Recipe } from '../shared/models/recipe.model';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [],
+  imports: [RecipeDetailsComponent,RecipeListComponent,FormsModule],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
-export class RecipesComponent {
-
+export class RecipesComponent  implements OnInit{
+  recipeSelected!:Recipe
+  constructor(){}
+ngOnInit(): void {
+  
+}
 }

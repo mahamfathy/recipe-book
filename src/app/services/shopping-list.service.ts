5d9b2eef84c0f7e1f060bee7a8beaeddef35d57e
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-28 03:04:48 +0200

    adjusting shopping.service services and shopping-list components

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/models/ingredients.models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
-
-  constructor() { }
+  ingredientsChanged = new EventEmitter<Ingredient[]>()
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomato', 10),
+  ];
+  constructor() {}
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+  ingredientAdded(Ingredient: Ingredient) {
+    this.ingredients.push(Ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice())
+  }
 }

5d6925a5828716654a80add8f81d8f5a3327a679
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-28 02:43:17 +0200

    adjusting recipe services and recipe components

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../shared/models/recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
- private recipes :Recipe[]=[
-    new Recipe("a test", "this is the first test","https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
-    new Recipe("another test", "this is the first test","https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg")
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'a test',
+      'this is the first test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+    new Recipe(
+      'another test',
+      'this is the first test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
   ];
-  constructor() { }
-  getRecipes(){
-    this.recipes.slice()
+  constructor() {}
+  getRecipes() {
+    return this.recipes.slice();
   }
 }

0797fd35e4914049212d5425cc99f70e05c448ed
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-01-31 21:58:36 +0200

    adding child routing

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { Recipe } from '../shared/models/recipe.model';
 import { RecipeDetailComponent } from './recipe-details/recipe-details.component';
 import { RecipeService } from '../services/recipe.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [RecipeDetailComponent, RecipeListComponent, FormsModule],
+  imports: [RecipeDetailComponent, RecipeListComponent, FormsModule,RouterOutlet],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css',
 })

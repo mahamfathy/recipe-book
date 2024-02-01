8a0722312ea302edea7ba1c1bc142debf5d7d29c
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-01 02:21:26 +0200

    editing controlling of recipe-edit component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 export const routes: Routes = [
 
 {path:'recipes',component:RecipesComponent,children:[
     {path:'',component:RecipeStartComponent},
+    {path:'new',component:RecipeEditComponent},
     {path:':id',component:RecipeDetailComponent},
+    {path:':id/edit',component:RecipeEditComponent},
 
 ]},
 {path:'shopping-list',component:ShoppingListComponent},

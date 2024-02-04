435188a51fb4b3635d2c15273ad1730a60532e84
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-04 23:50:19 +0200

    adding TD forms in shopping edit component

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -6,24 +6,21 @@ import {
 } from '@angular/core';
 import { Ingredient } from '../../shared/models/ingredients.models';
 import { ShoppingListService } from '../../services/shopping-list.service';
+import { FormControl, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './shopping-edit.component.html',
   styleUrls:[ './shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef!: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef!: ElementRef;
   constructor(private shoppingService:ShoppingListService) {}
   ngOnInit(): void {}
-  onAddItem(event: any) {
-    event.preventDefault();
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddItem(form:NgForm) {
+    const value = form.value
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingService.ingredientAdded(newIngredient)
   }
 }

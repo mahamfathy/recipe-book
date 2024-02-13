e8e1f1f5a61eb0a3cddf7bc6d2c6574d71b7e489
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-13 21:26:53 +0200

    subscribing signup  method in auth comp

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -12,12 +12,23 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
   isLoginMode = true;
-  constructor(private authService:AuthService){}
+  constructor(private authService: AuthService) {}
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
   onSubmit(authForm: NgForm) {
-    console.log(authForm);
+    if (authForm.invalid) {
+      return;
+    }
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+    if (this.isLoginMode) {
+    } else {
+      this.authService.signUp(email, password).subscribe((resData) => {
+        console.log(resData)
+      },error=>console.error(error)
+      );
+    }
     authForm.reset();
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthResponseData } from '../shared/models/auth-response-data.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,8 +9,8 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
-    return this.http.post(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[AIzaSyB0xX7FJdLBTD1M-Ohwc6fbHDB8ygUrbWM]',
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB0xX7FJdLBTD1M-Ohwc6fbHDB8ygUrbWM',
       {
         email: email,
         password: password,

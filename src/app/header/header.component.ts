d8b13f5c91d83bb29af0786d2a5715c694b51203
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-13 03:41:54 +0200

    adding fetch method in dataStorageService

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DataStorageService } from '../shared/models/data-storage.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -11,8 +10,9 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrls:[ './header.component.css'],
 })
 export class HeaderComponent {
-  constructor(private dataStorage: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService) {}
   onSaveData() {
-    this.dataStorage.storeData();
+    this.dataStorageService.storeData();
   }
+
 }
--- src/app/shared/models/data-storage.service.ts
+++ src/app/shared/models/data-storage.service.ts
@@ -5,19 +5,30 @@ import { RecipeService } from '../../services/recipe.service';
 @Injectable({
   providedIn: 'root',
 })
-export class DataStorageService{
+export class DataStorageService {
   constructor(private http: HttpClient, private recipeService: RecipeService) {}
   // storeData(recipes:Recipe[]){ or i can use another way to aviod arguments
   storeData() {
     const recipes = this.recipeService.getRecipes();
     // this is to add every recipe seperatly
-    // this.http.post('https://ng-recipe-book-ef4a9-default-rtdb.firebaseio.com/recipes.json',recipes) 
+    // this.http.post('https://ng-recipe-book-ef4a9-default-rtdb.firebaseio.com/recipes.json',recipes)
     //  but if i wanna override all the data i can use put method
-    this.http.put(
-      'https://ng-recipe-book-ef4a9-default-rtdb.firebaseio.com/recipes.json',
-      recipes
-    ).subscribe(response=>{
-        console.log(response)
-    })
+    this.http
+      .put(
+        'https://ng-recipe-book-ef4a9-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+  fetchData() {
+    this.http
+      .get(
+        'https://ng-recipe-book-ef4a9-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 }

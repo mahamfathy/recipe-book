248760c8e59800ce4a6379cd15d3515a43ef9b2e
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-13 19:40:21 +0200

    handling template driven form in auth comp

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
-isLoginMode = true;
-onSwitchMode(){
-  this.isLoginMode= !this.isLoginMode
-}
+  isLoginMode = true;
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+  onSubmit(authForm: NgForm) {
+    console.log(authForm);
+    authForm.reset();
+  }
 }

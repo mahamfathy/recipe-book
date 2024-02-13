7ebdc1139b1095b671e8757c0cb57c3f3404345a
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-13 02:58:28 +0200

    adding storeData method to the service

---
+++ src/app/shared/models/data-storage.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { RecipeService } from '../../services/recipe.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  // storeData(recipes:Recipe[]){ or i can use another way to aviod arguments
+  storeData() {
+    const recipes = this.recipeService.getRecipes();
+    // this is to add every recipe seperatly
+    // this.http.post('https://ng-recipe-book-ef4a9-default-rtdb.firebaseio.com/recipes.json',recipes) 
+    //  but if i wanna override all the data i can use put method
+    this.http.put(
+      'https://ng-recipe-book-ef4a9-default-rtdb.firebaseio.com/recipes.json',
+      recipes
+    ).subscribe(response=>{
+        console.log(response)
+    })
+  }
+}

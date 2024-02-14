23c66165748df838cd15f33731dff581230e12e4
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-14 22:04:47 +0200

    finish auth guard method

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,5 +1,13 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { AuthService } from './auth.service';
+import { map } from 'rxjs/operators';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  return authService.user.pipe(
+    map((user) => {
+      return !!user;
+    })
+  );
 };

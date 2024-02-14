dfd4b6066d55f5903165530214abe7338a0743c9
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-14 22:00:20 +0200

    generate auth guard

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -1,9 +1,9 @@
-import { HttpHandlerFn, HttpInterceptorFn, HttpParams, HttpRequest } from '@angular/common/http';
+import { HttpInterceptorFn, HttpParams } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
 import { exhaustMap, take } from 'rxjs/operators';
 
-export const authInterceptor: HttpInterceptorFn = (req:HttpRequest<any>, next:HttpHandlerFn) => {
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService)
   return authService.user.pipe(
     take(1),
